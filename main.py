7df77d4932ec8d2d13d9644ef037c5cba2e4b780
Zeid Abdulrazeq <zeid.abdulrazeq@outlook.com>
2022-02-27 17:33:50 -0500

    add delete api and define get post index used to delete in memory post

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from email.mime import base
 from os import stat
 from typing import Optional
+from urllib import response
 from fastapi import Body, FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from random import randrange
 
-#pydantic models to define the schema to insure validity
+
+
 class Post(BaseModel):
+    """ pydantic models to define the schema of the post """
     title: str
     content: str
     rating: Optional[int]
 
-app = FastAPI()
 
+app = FastAPI()
 
 
 my_posts = [
@@ -24,25 +27,29 @@ my_posts = [
     "id": 1}    
 ]
 
+
 def find_post(id):
+    """find post id from in memory list of dict"""
     for p in my_posts:
         if p['id'] == id:
             return p
 
-def find_index_post    
-#path operation in this case it's the root path ... decorator @ with get method 
-#doesnt need to be async
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 
-#order of functions matters when its the same path first will be executed and ended
+
 @app.get("/posts")
 def get_posts():
+    """get all posts"""
     return {"data": my_posts} #fast api will serialize the data into json  format
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED) #change from default 200 
-def create_post(post: Post): #post is a dytantic model
-    print()
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post): 
     post_dict = post.dict()
     post_dict['id'] = randrange(0,10000000)
     my_posts.append(post_dict)
@@ -50,11 +57,9 @@ def create_post(post: Post): #post is a dytantic model
 
 
 
-#get a specific post
-#path parameter are always returned as a string unless u spec type
 @app.get("/posts/{id}")
 def get_posts(id: int, response:Response):
-
+    """get specific post"""
     post = find_post(id)
     if not post:
         response.status_code = status.HTTP_404_NOT_FOUND
@@ -63,4 +68,10 @@ def get_posts(id: int, response:Response):
     return {'data':post}
 
 
-@app.delete('/posts/{id}')
+@app.delete('/posts/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    indx = find_index_post(id)
+    if indx == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="not a valid post id")
+    my_posts.pop(indx)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
